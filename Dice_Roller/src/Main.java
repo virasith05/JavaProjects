1dede4421270932ea9c05886bf2c4d59e6068232
virasith05 <virasith1234@gmail.com>
2025-01-19 22:35:36 +0530

    P10

---
+++ Dice_Roller/src/Main.java
@@ -0,0 +1,83 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        Scanner scanner=new Scanner(System.in);
+        Random random=new Random();
+        int roll;
+        int total =0;
+        System.out.print("Enter how many times u want to roll the dice: ");
+        int nofofdice= scanner.nextInt();
+
+        if (nofofdice>0){
+            for (int i=0;i<nofofdice;i++){
+                roll=random.nextInt(1,7);
+                System.out.println("You have rolled: "+roll);
+                dice(roll);
+                total+=roll;
+            }
+            System.out.println("Total: "+total);
+        }
+        else {
+            System.out.println("Invalid Number");
+        }
+
+        scanner.close();
+
+    }static void dice(int roll){
+        String dice1= """
+                 ---------------
+                |               |
+                |       ●       |
+                |               |   
+                 ---------------
+                """;
+        String dice2= """
+                 ---------------
+                |   ●           |
+                |               |
+                |            ●  |   
+                 ---------------
+                """;
+        String dice3= """
+                 ---------------
+                |   ●           |
+                |       ●       |
+                |           ●   |   
+                 ---------------
+                """;
+        String dice4= """
+                 ---------------
+                |   ●       ●   |
+                |               |
+                |   ●       ●   |   
+                 ---------------
+                """;
+        String dice5= """
+                 ---------------
+                |   ●       ●   |
+                |       ●       |
+                |   ●       ●   |   
+                 ---------------
+                """;
+        String dice6= """
+                 ---------------
+                |   ●       ●   |
+                |   ●       ●   |
+                |   ●       ●   |   
+                 ---------------
+                """;
+
+        switch (roll){
+            case 1 ->System.out.println(dice1);
+            case 2 ->System.out.println(dice2);
+            case 3 ->System.out.println(dice3);
+            case 4 ->System.out.println(dice4);
+            case 5 ->System.out.println(dice5);
+            case 6 ->System.out.println(dice6);
+            default -> System.out.println("Invalid");
+        }
+    }
+}
