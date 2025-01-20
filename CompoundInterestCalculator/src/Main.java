c8bf2b13306a94e1831c0f9bc905f7e51d763af6
virasith <118039006+virasith05@users.noreply.github.com>
2025-01-20 23:11:02 +0530

    Update Main.java

--- CompoundInterestCalculator/src/Main.java
+++ CompoundInterestCalculator/src/Main.java
@@ -3,14 +3,14 @@ public class Main {
     public static void main(String[] args){
         Scanner scanner=new Scanner(System.in);
         System.out.print("Enter Principal amount: ");
-        double p= scanner.nextInt();
+        double principal= scanner.nextInt();
         System.out.print("Enter Interest rate(in %): ");
         double r= scanner.nextDouble()/100;
         System.out.print("Enter the no of times compounded per year: ");
         int t= scanner.nextInt();
         System.out.print("Enter the no of years: ");
         int n= scanner.nextInt();
-        double f=p * Math.pow( (1+(r/n)),(n*t));
+        double f=principal * Math.pow( (1+(r/n)),(n*t));
         System.out.printf("Total Amount is: %.2f",f);
         scanner.close();
     }
