5c9ece441cc3fa54e88247539d2f9b23eeaf4647
virasith <118039006+virasith05@users.noreply.github.com>
2025-01-24 23:18:27 +0530

    Update Main.java

--- Quiz_Game/src/Main.java
+++ Quiz_Game/src/Main.java
@@ -5,15 +5,15 @@ public class Main {
 
         System.out.println("Hey,Hello Welcome to basic quiz game in Java");
         System.out.println("########################################################");
-        String[] questions={"A.Which of these is not a Java keyword?",
-        "B.What is the default value of an int variable in Java?",
-        "C.Which of the following is the correct way to declare a variable in Java?",
-        "D.Who is the founder of Java programming language?"};
+        String[] questions={"Q1.Which of these is not a Java keyword?",
+        "Q2.What is the default value of an int variable in Java?",
+        "Q3.Which of the following is the correct way to declare a variable in Java?",
+        "Q4.Who is the founder of Java programming language?"};
 
         String[][] answers={{"1.class", "2. interface", "3. main", "4. extends"},
                 {"1. 0", "2. null", "3. undefined", "4. Not assigned"},
                 {"1. int 1number;",  "2. float rate;", "3. boolean = true;" , "4. String 123Name;"},
-                {"1. Bjarne Stroustrup" , "B. Guido van Rossum" , "C. James Gosling" , "D. Dennis Ritchie"}};
+                {"1. Bjarne Stroustrup" , "2. Guido van Rossum" , "3. James Gosling" , "4. Dennis Ritchie"}};
 
         int[] options={3,1,2,3};
         int total=0;
